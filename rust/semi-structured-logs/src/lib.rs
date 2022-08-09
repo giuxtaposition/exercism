e6e2ef82a7adf8da50472b407762601bfbc72ab0
giuxtaposition <yg97.cs@gmail.com>
2022-08-09 14:59:43 +0200

    rust/semi-structured-logs

---
+++ rust/semi-structured-logs/src/lib.rs
@@ -0,0 +1,25 @@
+/// various log levels
+#[derive(Clone, PartialEq, Debug)]
+pub enum LogLevel {
+    Info,
+    Warning,
+    Error,
+}
+
+/// primary function for emitting logs
+pub fn log(level: LogLevel, message: &str) -> String {
+    match level {
+        LogLevel::Info => {info(message)},
+        LogLevel::Warning => {warn(message)}
+        LogLevel::Error => {error(message)}
+    }.to_string()
+}
+pub fn info(message: &str) -> String {
+    format!("[INFO]: {}", message)
+}
+pub fn warn(message: &str) -> String {
+    format!("[WARNING]: {}", message)
+}
+pub fn error(message: &str) -> String {
+    format!("[ERROR]: {}", message)
+}
